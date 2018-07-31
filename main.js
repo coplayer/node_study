75d09507b9a33972b5083764a5249f6af208aa49
HyunJin Yang <ally9489@gmail.com>
2018-07-31 09:27:22 +0900

    Sourcetree Test

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title , list, body){
           <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
+            <a href="/create">create</a>
             ${body}
           </body>
           </html>
@@ -36,7 +37,7 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-
+  console.log(pathname);
 
   if (pathname === '/') {
     if (queryData.id === undefined) {
